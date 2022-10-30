b6f6a584139ad9324610c54f2ac7b7351673b972
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-10-30 11:46:29 -0300

    Projeto Alura Tracker:
    incluindo ícones para navegação na barra lateral

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,29 @@
 <template>
     <header>
         <h1>
-            <img src="../assets/logo.png" alt="">
+            <img src="../assets/logo.png" alt="logo da Alura Tracker">
         </h1>
-        <button class="button" @click="alterarTema"> 
-            {{ textoBotado }}
-        </button>
+        <div class="has-text-centered">
+            <button class="button" @click="alterarTema"> 
+                {{ textoBotado }}
+            </button>
+        </div>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <router-link to="/" class="link">
+                      <i class="fas fa-tasks"></i>
+                      tarefas 
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/projetos" class="link">
+                      <i class="fas fa-project-diagram"></i>
+                      projetos
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -44,6 +62,22 @@
         height: 100vh;
         padding: 1rem;
         text-align: center;
+    }
+
+    .panel li {
+        margin: 8px 0;
+    }
+
+    .link {
+        color: #FFFFFF;
+    }
+
+    .link:hover {
+        color: #FAF0CA;
+    }
+
+    .link.router-link-active {
+        color: #FAF0CA
     }
 
     @media only screen and (max-width: 768px) {
