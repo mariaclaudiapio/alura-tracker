a736c9aeb3d81f4fa6917feedcf65e61802a3bbb
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-11-01 12:02:52 -0300

    Projeto Alura Tracker
    implantação da funcionalidade de editar projeto

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -23,7 +23,8 @@ const rotas: RouteRecordRaw[] = [
     {
         path: '/projetos/:id',
         name: 'Editar projeto',
-        component: Formulario
+        component: Formulario,
+        props: true
     }, 
 
 ]
