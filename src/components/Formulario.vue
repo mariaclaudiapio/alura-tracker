dfc5431597c565edbcfeeda5150886aa787088bb
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-11-06 11:54:54 -0300

    Projeto Alura Tracker:
    refatorando componentes com Composition API

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -29,7 +29,7 @@
 
 <script lang="ts">
     import { key } from '@/store'
-    import { computed, defineComponent } from 'vue'
+    import { computed, defineComponent, ref } from 'vue'
     import { useStore } from 'vuex'
 
     import Temporizador from './Temporizador.vue'
@@ -40,26 +40,30 @@
         components: {
             Temporizador
         },
-        data() {
-            return {
-                descricao: '',
-                idProjeto: ''
-            }
-        },
-        methods: {
-            finalizarTarefa (tempoDecorrido: number) : void {
-                this.$emit('aoSalvarTarefa', {
-                    duracaoEmSegundos: tempoDecorrido,
-                    descricao: this.descricao,
-                    projeto: this.projetos.find(proj => proj.id == this.idProjeto )
-                })
-                this.descricao = ''
-            }
-        },
-        setup() {
+        setup(props, { emit }) {
+
             const store = useStore(key)
+
+            const descricao = ref("")
+            const idProjeto = ref("")
+
+            const projetos = computed(() => store.state.projeto.projetos)
+
+            const finalizarTarefa = (tempoDecorrido: number) : void => {
+                    emit('aoSalvarTarefa', {
+                    duracaoEmSegundos: tempoDecorrido,
+                    descricao: descricao.value,
+                    projeto: projetos.value.find(proj => proj.id == idProjeto.value )
+                })
+                descricao.value = ''
+            }
+
             return {
-                projetos: computed(() => store.state.projeto.projetos)
+                descricao,
+                idProjeto,
+                projetos,
+                finalizarTarefa
+                
             }
         }
     })
